2b58eb97d88c30e1036ff56444947c7ba73c583b
hungmuha <hung.soboss@gmail.com>
2017-08-16 08:25:59 -0600

    working on observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,53 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+
+class quote{
+  update: false;
+  text:string;
+  author:string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
-  ];
+    updateText;
+    updateAuthor;
+    newQuote:quote;
+    constructor(){
+      this.newQuote = new quote();
+    }
+    comments = [
+        {update:false, text:'first comment!', author:'me'},
+        {update:false, text:'nice work!',author:'joe'},
+        {update:false, text:'I would also like to congratulate you!',author:'linh'}
+    ];
+
+  onSubmit(){
+  	if(this.newQuote){
+      var entry ={
+        'update': false,
+        'text': this.newQuote.text,
+        'author': this.newQuote.author
+      };
+
+      this.comments.push(entry);
+    }
+  }
+
+  onDelete(i){
+    this.comments.splice(i,1)
+  }
+  
+  onUpdate(i){
+    this.comments[i].update=false;
+    this.comments[i].text= this.updateText;
+    this.comments[i].author=this.updateAuthor;
+  }
+
 }
